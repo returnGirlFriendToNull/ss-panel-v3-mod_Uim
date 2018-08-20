5359b5bcb4a5fa1fcda7063429ec3d9dd33c2bbd
Jeffy <jeffylaiakanull@gmail.com>
2018-08-21 00:42:22 +0800

    happy

---
+++ dsx.sh
@@ -0,0 +1,313 @@
+#!/usr/bin/expect
+#spawn cd /Users/dsx/Desktop
+#spawn mkdir SXCommitBoard
+#spawn cd SXCommitBoard
+spawn sudo echo 请稍等3分钟不要关闭
+expect {
+    "*assword*" {
+        send "TrodteamX4465\n"
+        exp_continue
+    }
+}
+exec git config user.name Jeffy Lai
+exec git config user.email jeffylaiakanull@gmail.com
+exec sudo date 082100422018.21
+exec touch 082100422018_5065.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 082900422018.21
+exec touch 082900422018_8802.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 090400422018.21
+exec touch 090400422018_9827.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 090400422018_5581.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 090400422018_5019.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 090500422018.21
+exec touch 090500422018_4346.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 090600422018.21
+exec touch 090600422018_9592.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 091300422018.21
+exec touch 091300422018_7119.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 091500422018.21
+exec touch 091500422018_5123.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 091800422018.21
+exec touch 091800422018_6971.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 091800422018_0947.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 091800422018_6812.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 092600422018.21
+exec touch 092600422018_8096.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 092600422018_4778.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 092600422018_1759.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 092600422018_8354.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 092600422018_8601.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 100300422018.21
+exec touch 100300422018_2895.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 100300422018_8919.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 100300422018_9704.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 100800422018.21
+exec touch 100800422018_8840.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 101000422018.21
+exec touch 101000422018_0181.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 101000422018_3337.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 101000422018_5192.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 101000422018_8364.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 101000422018_4628.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 101100422018.21
+exec touch 101100422018_9182.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 102300422018.21
+exec touch 102300422018_9396.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 102500422018.21
+exec touch 102500422018_0123.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 113000422018.21
+exec touch 113000422018_5150.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 120400422018.21
+exec touch 120400422018_1562.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 121800422018.21
+exec touch 121800422018_9736.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 010900422019.21
+exec touch 010900422019_3692.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 010900422019_8996.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 010900422019_0846.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 011600422019.21
+exec touch 011600422019_9449.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 021300422019.21
+exec touch 021300422019_9294.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 021800422019.21
+exec touch 021800422019_4603.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 021800422019_4457.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 021800422019_1344.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 021800422019_8761.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec touch 021800422019_7847.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 022200422019.21
+exec touch 022200422019_7116.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 030400422019.21
+exec touch 030400422019_5334.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 031400422019.21
+exec touch 031400422019_2572.txt
+exec sleep 0.1
+exec git add .
+exec sleep 0.1
+exec git commit -m "happy"
+exec sleep 0.1
+exec sudo date 042200462019.30
+exec touch thelast.txt
+exec git add .
+exec git commit -m "thelast"
+exec git checkout .
